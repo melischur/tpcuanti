c807e1ac41d18bb571fe948d006a501f550ff0e0
SCHURMAN MELINA <mschurman@estudiantes.unsam.edu.ar>
2024-09-02 02:19:44 -0300

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,7 @@
+# tpcuanti
+# tpcuanti
+# Pseudocódigo:
+# 1. Abrir el archivo y leer los porcentajes.
+# 2. Convertir los porcentajes a números.
+# 3. Calcular el promedio de los porcentajes.
+# 4. Imprimir el porcentaje general.
