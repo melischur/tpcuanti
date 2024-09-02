2e0c6b6eda68a5f66cb255031f05a9ef3cd80f59
SCHURMAN MELINA <mschurman@estudiantes.unsam.edu.ar>
2024-09-02 03:29:55 -0300

    Update README.py

--- README.py
+++ README.py
@@ -7,7 +7,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from tkinter import Tk, Button, filedialog
+import streamlit as st
 
 def leer_datos(archivo):
     # Leer el archivo CSV
@@ -21,9 +21,9 @@ def procesar_datos(datos):
             # Intentar convertir la columna 'fecha' a formato datetime
             datos['fecha'] = pd.to_datetime(datos['fecha'])
         except Exception as e:
-            print(f"Error al convertir la columna 'fecha' a datetime: {e}")
+            st.error(f"Error al convertir la columna 'fecha' a datetime: {e}")
     else:
-        print("La columna 'fecha' no se encuentra en los datos.")
+        st.warning("La columna 'fecha' no se encuentra en los datos.")
         # Opcional: Manejar la ausencia de la columna 'fecha'
         # Por ejemplo, podrías crear una columna 'fecha' vacía o con valores predeterminados
         # datos['fecha'] = pd.NaT  # NaT significa "Not a Time" en pandas
@@ -33,25 +33,21 @@ def procesar_datos(datos):
 
 def generar_grafico(porcentaje_llamados):
     # Generar el gráfico de barras
-    porcentaje_llamados.plot(kind='bar')
-    plt.xlabel('Mes')
-    plt.ylabel('Porcentaje de Llamados')
-    plt.title('Porcentaje de Llamados por Casos COVID por Mes')
-    plt.show()
+    fig, ax = plt.subplots()
+    porcentaje_llamados.plot(kind='bar', ax=ax)
+    ax.set_xlabel('Mes')
+    ax.set_ylabel('Porcentaje de Llamados')
+    ax.set_title('Porcentaje de Llamados por Casos COVID por Mes')
+    st.pyplot(fig)
 
-def cargar_archivo():
-    archivo = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
-    if archivo:
+def main():
+    st.title("Análisis de casos COVID 2021")
+    archivo = st.file_uploader("Cargar Archivo CSV", type="csv")
+    
+    if archivo is not None:
         datos = leer_datos(archivo)
         porcentaje_llamados = procesar_datos(datos)
         generar_grafico(porcentaje_llamados)
-
-def main():
-    root = Tk()
-    root.title("Análisis de casos COVID 2021")
-    boton_cargar = Button(root, text="Cargar Archivo CSV", command=cargar_archivo)
-    boton_cargar.pack(pady=20)
-    root.mainloop()
 
 if __name__ == '__main__':
     main()
